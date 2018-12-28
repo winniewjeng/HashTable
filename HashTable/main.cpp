476e9cc5f75de4b11a4e22ea10c9552796f8a1d0
winniewjeng <winniewjeng@gmail.com>
2018-12-28 13:23:51 -0800

    Initial Commit

---
+++ HashTable/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HashTable
+//
+//  Created by Winnie Jeng on 12/28/18.
+//  Copyright © 2018 Winnie Jeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
