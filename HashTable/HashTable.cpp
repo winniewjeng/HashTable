ddfe1cea6c09cfd1bf3f0f8091689eea43639012
winniewjeng <winniewjeng@gmail.com>
2018-12-28 16:53:12 -0800

    first commit

---
+++ HashTable/HashTable.cpp
@@ -0,0 +1,9 @@
+//
+//  HashTable.cpp
+//  HashTable
+//
+//  Created by Winnie Jeng on 12/28/18.
+//  Copyright © 2018 Winnie Jeng. All rights reserved.
+//
+
+#include "HashTable.hpp"
---
+++ HashTable/HashTable.hpp
@@ -0,0 +1,14 @@
+//
+//  HashTable.hpp
+//  HashTable
+//
+//  Created by Winnie Jeng on 12/28/18.
+//  Copyright © 2018 Winnie Jeng. All rights reserved.
+//
+
+#ifndef HashTable_hpp
+#define HashTable_hpp
+
+#include <stdio.h>
+
+#endif /* HashTable_hpp */
